d81aaa43a101a6b2bf4cae2ba750e1786de5bf74
RakaAPratama <r.andinan@mail.ugm.ac.id>
2022-06-08 09:33:27 +0700

    init

---
+++ src/main/java/com/toko/kelontong/KelontongApplication.java
@@ -0,0 +1,13 @@
+package com.toko.kelontong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KelontongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KelontongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toko/kelontong/KelontongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toko.kelontong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KelontongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
